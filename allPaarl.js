4edd380c05ea618949222e5257eddf1be42a392c
antselley@gmail.com <antselley@gmail.com>
2016-10-07 12:40:13 +0200

    All from Paarl figured out

---
+++ allPaarl.js
@@ -0,0 +1,18 @@
+var AllPaarl = function(string) {
+  // Turn the string list into an array//
+  var array = string.split(",");
+
+// Trim out unneeded spaces //
+  for (item in array) {
+    array[item] = array[item].trim();
+  }
+
+// Finding and printing the first one from paarl...
+  for (item in array) {
+    if (array[item].startsWith('CJ')) {
+      console.log(array[item]);
+    }
+  }
+}
+
+AllPaarl("CA 453 928, CJ 348 293 ,274 239 GP, CJ 219 549") ;

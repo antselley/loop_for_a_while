41f29e121b03dca97bec10a6084da8d56b741ec1
antselley@gmail.com <antselley@gmail.com>
2016-10-07 12:07:49 +0200

    Shopping list done

--- shoppingList.js
+++ shoppingList.js
@@ -1,2 +1,9 @@
-Testing
-again.
+var shoppingLIst = function(string) {
+  var array = string.split(",");
+  console.log(array);
+  for (item in array) {
+    console.log(array[item]);
+  }
+}
+
+shoppingLIst("Butter,Raisin Bread,Cheese,Coffee") ;
